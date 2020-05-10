bb5d11b8e339496ee0ec741d2a3be3aef86a5429
Alex Ballera <alexballera@gmail.com>
2020-05-09 23:44:05 -0300

    Agregada rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
